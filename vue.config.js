3428da28cf373c57959b0b38b380ebc129d94422
Keosh <106485828+keosh1@users.noreply.github.com>
2025-06-16 19:21:13 +0300

    feat: migrate to dopaemon fastboot.js implementation

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,32 @@ module.exports = {
         process.env.NODE_ENV === "production"
             ? process.env.VUE_APP_PROD_URL
             : "/",
-    transpileDependencies: ["vuetify"],
+    transpileDependencies: ["vuetify", "android-fastboot"],
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /fastboot\.cjs$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                    targets: {
+                                        browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
+                                    }
+                                }]
+                            ],
+                            plugins: [
+                                '@babel/plugin-transform-optional-chaining',
+                                '@babel/plugin-transform-nullish-coalescing-operator'
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    },
     chainWebpack: (config) => {
         config.plugin("html").tap((args) => {
             args[0].title = `${process.env.VUE_APP_OS_NAME} web installer`;
